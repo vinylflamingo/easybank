a28f1eb0a5d616b59d4440b5f4c04821cfdeac03
Frank Costoya <66138552+vinylflamingo@users.noreply.github.com>
2023-07-08 20:25:16 -0400

    footer complete

--- src/easybank/src/app/components/footer/footer.component.ts
+++ src/easybank/src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ComponentRendering } from '@sitecore-jss/sitecore-jss-angular';
+import { Item } from '@sitecore-jss/sitecore-jss/layout';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,23 @@ export class FooterComponent implements OnInit {
   @Input() rendering: ComponentRendering;
 
   constructor() { }
+
+  chunk(arr: Item[], size: number): Item[][] {
+    if (!arr.length) {
+      return [];
+    }
+  
+    const head = arr.slice(0, size);
+    const tail = arr.slice(size);
+  
+    return [head, ...this.chunk(tail, size)];
+  }
+  
+  get linkGroups(): Item[][] {
+    return this.chunk(this.rendering.fields.links as Item[], 3);
+  }
+  
+
 
   ngOnInit() {
     // remove this after implementation is done
